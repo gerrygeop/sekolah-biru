532b04bc91ea876f26f31b25eda66daec4fa2cd5
gerrygeop <gerryja28@gmail.com>
2025-12-03 00:13:41 +0800

    refactor: update contact page

--- app/Livewire/Contact/Index.php
+++ app/Livewire/Contact/Index.php
@@ -3,11 +3,23 @@
 namespace App\Livewire\Contact;
 
 use Livewire\Component;
+use App\Models\ContactInfo;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class Index extends Component
 {
+    public function mount()
+    {
+        SEOTools::setTitle('Kontak - SMP Digital', false);
+        SEOTools::setDescription('Hubungi kami untuk informasi lebih lanjut tentang SMP Digital');
+        SEOTools::opengraph()->setUrl(url()->current());
+        SEOTools::opengraph()->addProperty('type', 'website');
+    }
+
     public function render()
     {
-        return view('livewire.contact.index');
+        return view('livewire.contact.index', [
+            'contact' => ContactInfo::first()
+        ]);
     }
 }
