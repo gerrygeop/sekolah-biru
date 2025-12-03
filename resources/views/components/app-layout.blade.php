2daae064f7dcaac116590afdd81262bfc6bd6f97
gerrygeop <gerryja28@gmail.com>
2025-12-03 15:43:10 +0800

    refactor: add x-cloak directive for smoother dropdown transitions

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -46,7 +46,7 @@
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
 							</svg>
 						</button>
-						<div x-show="open" x-transition:enter="transition ease-out duration-200"
+						<div x-cloak x-show="open" x-transition:enter="transition ease-out duration-200"
 							x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
 							class="absolute left-0 top-full w-56 bg-white rounded-lg shadow-lg border border-gray-200 py-2">
 							<a href="{{ route('profile.vision-mission') }}"
@@ -71,7 +71,7 @@
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
 							</svg>
 						</button>
-						<div x-show="open" x-transition:enter="transition ease-out duration-200"
+						<div x-cloak x-show="open" x-transition:enter="transition ease-out duration-200"
 							x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
 							class="absolute left-0 top-full w-56 bg-white rounded-lg shadow-lg border border-gray-200 py-2">
 							<a href="{{ route('graduates.achievements') }}"
@@ -97,7 +97,7 @@
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
 							</svg>
 						</button>
-						<div x-show="open" x-transition:enter="transition ease-out duration-200"
+						<div x-cloak x-show="open" x-transition:enter="transition ease-out duration-200"
 							x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
 							class="absolute left-0 top-full w-48 bg-white rounded-lg shadow-lg border border-gray-200 py-2">
 							<a href="{{ route('gallery.photos') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50">Foto</a>
