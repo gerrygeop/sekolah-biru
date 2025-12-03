8c44d376611e042ca6016c7cfe45cb607275b5ed
gerrygeop <gerryja28@gmail.com>
2025-12-03 16:45:23 +0800

    refactor: enhance Photos and Videos components for improved data handling and rendering

--- app/Livewire/Gallery/Photos.php
+++ app/Livewire/Gallery/Photos.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Gallery;
 
+use App\Services\GalleryService;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Livewire\Component;
 
 class Photos extends Component
 {
+    protected $galleryService;
+
+    public function mount()
+    {
+        $this->galleryService = app(GalleryService::class);
+        SEOMeta::setTitle('Galeri Foto - SMP Digital', false);
+    }
+
     public function render()
     {
-        return view('livewire.gallery.photos');
+        return view('livewire.gallery.photos', [
+            'photos' => $this->galleryService->getAllPhotos(12),
+        ]);
     }
 }
--- app/Livewire/Gallery/Videos.php
+++ app/Livewire/Gallery/Videos.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Gallery;
 
+use App\Services\GalleryService;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Livewire\Component;
 
 class Videos extends Component
 {
+    protected $galleryService;
+
+    public function mount()
+    {
+        $this->galleryService = app(GalleryService::class);
+        SEOMeta::setTitle('Galeri Video - SMP Digital', false);
+    }
+
     public function render()
     {
-        return view('livewire.gallery.videos');
+        return view('livewire.gallery.videos', [
+            'videos' => $this->galleryService->getAllVideos(12),
+        ]);
     }
 }
