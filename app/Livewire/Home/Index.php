43bb85703f2b54bb46294911a2a714225824bd40
gerrygeop <gerryja28@gmail.com>
2025-12-03 15:22:24 +0800

    refactor: enhance SEO settings and clean up layout in homepage

--- app/Livewire/Home/Index.php
+++ app/Livewire/Home/Index.php
@@ -13,7 +13,7 @@ class Index extends Component
     public function mount()
     {
         // Set SEO for homepage
-        SEOTools::setTitle('Beranda - SMP Digital');
+        SEOTools::setTitle('Beranda - SMP Digital', false);
         SEOTools::setDescription('Website resmi SMP Digital - Sekolah Menengah Pertama unggul dalam prestasi dan berkarakter');
         SEOTools::opengraph()->setUrl(url()->current());
         SEOTools::opengraph()->addProperty('type', 'website');
@@ -24,22 +24,22 @@ class Index extends Component
         $newsService = app(NewsService::class);
         $studentService = app(StudentService::class);
         $schoolService = app(SchoolProfileService::class);
-        
+
         $latestNews = $newsService->getLatestNews(4);
         $featuredAchievements = $studentService->getAchievements(null, 3);
         $currentYear = $studentService->getCurrentAcademicYear();
         $schoolProfile = $schoolService->getSchoolProfile();
-        
+
         $stats = null;
         if ($currentYear) {
             $statistics = $studentService->getStudentStatistics($currentYear->id);
-            $totalStudents = $statistics->sum(function($stat) {
+            $totalStudents = $statistics->sum(function ($stat) {
                 return $stat->male_count + $stat->female_count;
             });
-            
+
             $employees = $schoolService->getEmployees('guru');
             $achievements = $studentService->getAchievements(null, null);
-            
+
             $stats = [
                 'students' => $totalStudents,
                 'teachers' => $employees->count(),
@@ -52,6 +52,6 @@ class Index extends Component
             'featuredAchievements' => $featuredAchievements,
             'schoolProfile' => $schoolProfile,
             'stats' => $stats,
-        ])->layout('layouts.app');
+        ]);
     }
 }
